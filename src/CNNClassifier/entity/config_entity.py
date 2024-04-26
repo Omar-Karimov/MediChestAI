3bfe9340852a0e0b56a8eed8af01dbd36d767e8d
Omar-Karimov <omarkerimov303@gmail.com>
2024-04-26 20:13:21 +0400

    Expand config entities in CNNClassifier with PrepareBaseModelConfig

--- src/CNNClassifier/entity/config_entity.py
+++ src/CNNClassifier/entity/config_entity.py
@@ -11,3 +11,13 @@ class DataIngestionConfig:
 
 
 
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
