ddd1e0932577d26777bddb78a71f4a516080dba7
Omar-Karimov <omarkerimov303@gmail.com>
2024-04-26 23:38:10 +0400

    Update configuration.py file

--- src/CNNClassifier/config/configuration.py
+++ src/CNNClassifier/config/configuration.py
@@ -62,7 +62,7 @@ class ConfigurationManager:
         training = self.config.training
         prepare_base_model = self.config.prepare_base_model
         params = self.params
-        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "Chest-CT-Scan-data")
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "dataset_chest")
         create_directories([
             Path(training.root_dir)
         ])
