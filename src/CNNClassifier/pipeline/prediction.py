83483d19596d298f5a2d285ef09eb48b35942707
Omar-Karimov <omarkerimov303@gmail.com>
2024-04-29 03:30:19 +0400

    Adding normalization layer in the predict function

--- src/CNNClassifier/pipeline/prediction.py
+++ src/CNNClassifier/pipeline/prediction.py
@@ -21,6 +21,7 @@ class PredictionPipeline:
         test_image = image.load_img(imagename, target_size = (224,224))
         test_image = image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis = 0)
+        test_image /= 255.0
         result = np.argmax(model.predict(test_image), axis=1)
         print(result)
 
